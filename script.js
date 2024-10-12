cff8c3a26251bd637401122dd67d8d1f6aa338f1
RedFox499 <50075311+RedFox499@users.noreply.github.com>
2024-10-13 02:30:44 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.getElementById("tema").addEventListener("click", function() {
   */
   const themeIcon = document.querySelector('#tema img');
   const themeLogo = document.querySelector('#logo');
+  const themeLogo2 = document.querySelector('#logoUnd');
   const arrows = document.querySelector('#arrowd');
   const arrowsd2 = document.querySelector('#arrowd2');
   const body = document.body;
@@ -22,10 +23,14 @@ document.getElementById("tema").addEventListener("click", function() {
   const isDarkTheme = body.classList.toggle('light-theme'); 
   themeIcon.src = isDarkTheme ? 'SUN.png' : 'MOON.png';
   themeLogo.src = isDarkTheme ? 'Rectangle69.png' : 'Rectangle.svg';
+  themeLogo2.src = isDarkTheme ? 'Rectangle69.png' : 'Rectangle.svg';
   arrows.src = isDarkTheme ? 'Vector.png' : 'arrow.svg';
   arrowsd2.src = isDarkTheme ? 'Vector.png' : 'arrow.svg';
-  body.style.transition = '0.25s';
-
- 
-  });
   
+  body.style.transition = '0.25s';
+  });
+ 
+
+   
+
+
